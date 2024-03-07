96d3e5dea7158735e14dee5e81718660762196c9
Dimitrios Zervas <dimitri_zervas@hotmail.com>
2024-03-07 15:48:53 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
 import './App.css';
 
+// fucntion to generate n keys.
+const generateNKeys = async (n, salt, type, baseKey) => {
+  try {
+    
+    let derivedKeyAlgo, keyUsage, info;
+    if (type === "sign") {
+      // if we want to create keys which will be used to sign the data
+      derivedKeyAlgo = { name: "HMAC", hash: "SHA-256", length: 256 };
+      keyUsage = ["sign", "verify"];    
+      info = new TextEncoder().encode("signs");
+    } else {
+      derivedKeyAlgo = { name: "AES-GCM", length: 256 };
+      keyUsage = ["encrypt", "decrypt"];     
+      info = new TextEncoder().encode("encrypts");
+    }
+    let keys = []; // array to store n keys
+
+    // for loop to generate n keys
+    for (let i = 0; i < n; i++) {
+      // here we are creating a key using HKDF algorithm
+      const key = await window.crypto.subtle.deriveKey(
+        {
+          name: "HKDF",
+          hash: "SHA-256",
+          salt: salt,
+          info: info
+        },
+        baseKey,
+        derivedKeyAlgo,
+        true,
+        keyUsage
+      );
+      keys.push(key);
+    }
+    return keys;
+  } catch (error) {
+    console.error("Error in generateNKeys:", error.message);
+    throw error;
+  }
+};
+
 function App() {
+  
+  async function handleClick() {
+  
+    const secretString = "secret";
+    let saltString  = "";
+
+    console.log("secret string: ", secretString);
+    
+    let secretRaw = new TextEncoder().encode(secretString);
+    let salt = new TextEncoder().encode(saltString);
+
+    console.log("secret: ", secretRaw);
+
+    // generateKey cannot be used to create a key which will be used to drive other keys in future so using importKey function
+    let secret = await window.crypto.subtle.importKey("raw", secretRaw, "HKDF", false, [
+      "deriveBits",
+      "deriveKey",
+    ]);
+    console.log("secret or baseKey: ", secret);
+  
+      // creating SRC from the SECRET
+    let srcAB = await window.crypto.subtle.deriveBits(
+      {
+        name: "HKDF",
+        hash: "SHA-256",
+        salt: salt,
+        info: new TextEncoder().encode("src"),
+      },
+      secret,
+      64
+    );
+
+    let src = new Uint8Array(srcAB);
+
+    salt = src;
+
+    console.log("src: ", src);
+
+    // Derive sign Key from secret
+    let signAB = await window.crypto.subtle.deriveBits(
+      {
+        name: "HKDF",
+        hash: "SHA-256",
+        salt: salt,
+        info: new TextEncoder().encode("sign"),
+      },
+      secret,      
+      256
+    );     
+
+    let signRaw = new Uint8Array(signAB);
+
+    console.log("sign: ", signRaw);
+
+    let sign = await window.crypto.subtle.importKey("raw", signRaw, "HKDF", false, [
+      "deriveKey",
+    ]);
+
+    // Derive encrypt Key from secret
+    let encryptAB = await window.crypto.subtle.deriveBits(
+      {
+        name: "HKDF",
+        hash: "SHA-256",
+        salt: salt,
+        info: new TextEncoder().encode("encrypt"),
+      },
+      secret,      
+      256
+    );     
+
+    let encryptRaw = new Uint8Array(encryptAB);
+
+    console.log("encrypt: ", encryptRaw);
+
+    let encrypt = await window.crypto.subtle.importKey("raw", encryptRaw, "HKDF", false, [  
+      "deriveKey",
+    ]);
+    
+    let n = 3;
+    const encrypts = await generateNKeys(n, salt, "encrypt", encrypt);      
+    const signs = await generateNKeys(n, salt, "sign", sign);
+
+    console.log("encrypts: ");
+    for (let i=0; i < encrypts.length; i++) {
+      let raw = new Uint8Array(await window.crypto.subtle.exportKey("raw", encrypts[i]));
+      console.log("encrypt[", i, "]: ", raw);
+    }
+
+    console.log("signs: ");
+    for (let i=0; i < signs.length; i++) {
+      let raw = new Uint8Array(await window.crypto.subtle.exportKey("raw", signs[i]));
+      console.log("sign[", i, "]: ", raw);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={handleClick} style={{
+      textAlign: 'center',
+      width: '100px',
+      border: '1px solid gray',
+      borderRadius: '5px'
+    }}>
+      Send data to backend
     </div>
   );
 }
